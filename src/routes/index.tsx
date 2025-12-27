3d88641760bf51c5b25e57008835648e95e6d76d
Kristopher Mendoza <creative.kristopher@gmail.com>
2025-12-27 19:25:06 +0800

    feat: extract fetch functions to ideas.ts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
-  component: App,
+  component: HomePage,
 });
 
-function App() {
+function HomePage() {
   return <>My App</>;
 }
