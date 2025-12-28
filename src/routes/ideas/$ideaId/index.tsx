306b51a3f5be7f97da471f4d3b92bc40493ca320
Kristopher Mendoza <creative.kristopher@gmail.com>
2025-12-28 23:04:56 +0800

    feat: implement idea deletion

--- src/routes/ideas/$ideaId/index.tsx
+++ src/routes/ideas/$ideaId/index.tsx
@@ -1,6 +1,11 @@
 import { createFileRoute, Link } from '@tanstack/react-router';
-import { queryOptions, useSuspenseQuery } from '@tanstack/react-query';
-import { fetchIdea } from '@/api/ideas';
+import { useNavigate } from '@tanstack/react-router';
+import {
+  queryOptions,
+  useSuspenseQuery,
+  useMutation,
+} from '@tanstack/react-query';
+import { fetchIdea, deleteIdea } from '@/api/ideas';
 
 const ideaQueryOptions = (ideaId: string) =>
   queryOptions({
@@ -20,6 +25,23 @@ function IdeaDetailsPage() {
 
   const { data: idea } = useSuspenseQuery(ideaQueryOptions(ideaId));
 
+  const navigate = useNavigate();
+
+  const { mutateAsync: deleteMutate, isPending } = useMutation({
+    mutationFn: () => deleteIdea(ideaId),
+    onSuccess: () => {
+      navigate({ to: '/ideas' });
+    },
+  });
+
+  const handleDelete = async () => {
+    const confirmDelete = window.confirm(
+      'Are you sure you want to delete this idea?'
+    );
+
+    if (confirmDelete) await deleteMutate();
+  };
+
   return (
     <div className="p-4">
       <Link to="/ideas" className="text-blue-500 underline block mb-4">
@@ -27,6 +49,13 @@ function IdeaDetailsPage() {
       </Link>
       <h2 className="text-2xl font-bold">{idea.title}</h2>
       <p className="mt-2">{idea.description}</p>
+      <button
+        onClick={handleDelete}
+        disabled={isPending}
+        className="text-sm bg-red-600 text-white mt-4 px-4 py-2 rounded transition hover:bg-red-700 disabled:opacity-50"
+      >
+        {isPending ? 'Deleting...' : 'Delete'}
+      </button>
     </div>
   );
 }
