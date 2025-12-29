52addf9373d564940c6f36246b36de8eceea784d
Kristopher Mendoza <creative.kristopher@gmail.com>
2025-12-30 05:20:44 +0800

    refactor: update and limit through the backend

--- src/routes/ideas/index.tsx
+++ src/routes/ideas/index.tsx
@@ -6,7 +6,7 @@ import IdeaCard from '@/components/IdeaCard';
 const ideasQueryOptions = () =>
   queryOptions({
     queryKey: ['ideas'],
-    queryFn: fetchIdeas,
+    queryFn: () => fetchIdeas(),
   });
 
 export const Route = createFileRoute('/ideas/')({
@@ -24,10 +24,7 @@ export const Route = createFileRoute('/ideas/')({
 });
 
 function IdeasPage() {
-  const { data } = useSuspenseQuery(ideasQueryOptions());
-  const ideas = [...data].sort(
-    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-  );
+  const { data: ideas } = useSuspenseQuery(ideasQueryOptions());
 
   return (
     <div className="p-4">
