b73be4aaada20f45d662300633207da2d8bb7fa1
Kristopher Mendoza <creative.kristopher@gmail.com>
2026-01-02 20:42:44 +0800

    feat: refresh access token on page reload

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,11 @@
-import { createContext, useContext, useState, type ReactNode } from 'react';
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  type ReactNode,
+} from 'react';
+import { refreshAccessToken } from '@/api/auth';
 
 type AuthContextType = {
   accessToken: string | null;
@@ -16,6 +23,20 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [accessToken, setAccessToken] = useState<string | null>(null);
   const [user, setUser] = useState<AuthContextType['user'] | null>(null);
+
+  useEffect(() => {
+    const loadAuth = async () => {
+      try {
+        const { accessToken: newToken, user } = await refreshAccessToken();
+        setAccessToken(newToken);
+        setUser(user);
+      } catch (err: any) {
+        console.log('Failed to refresh token', err);
+      }
+    };
+
+    loadAuth();
+  }, []);
 
   return (
     <AuthContext.Provider
